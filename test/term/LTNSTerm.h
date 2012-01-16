bd3ac4be12c2c630827b15a871bcb43bc7585384
Christian Lundgren <christian.lundgren@wooga.net>
2012-01-16 11:19:40 +0100

    Meeting notes

--- test/term/LTNSTerm.h
+++ test/term/LTNSTerm.h
@@ -30,29 +30,10 @@ int LTNSDestroyTerm( LTNSTerm *term );
 // setter and getter for raw data
 int LTNSSetData( LTNSTerm *term, char* data, size_t length );
 int LTNSGetData( LTNSTerm *term, char* data, size_t *length );
+// TODO: implement payload length and term length functions
 int LTNSGetValueLength( LTNSTerm *term, size_t* length );
 LTNSType LTNSGetType( LTNSTerm *term );
 
 // setter and getter for value data
-int LTNSSetInteger( LTNSTerm *term, int value );
-int LTNSGetInteger( LTNSTerm *term, int* value );
-
-int LTNSSetString( LTNSTerm *term, char* value, size_t length );
-int LTNSGetString( LTNSTerm *term, char* value, size_t* length );
-
-int LTNSSetBoolean( LTNSTerm *term, int value );
-int LTNSGetBoolean( LTNSTerm *term, int* value );
-
-int LTNSSetNull( LTNSTerm *term );
-
-int LTNSSetList( LTNSTerm *term, char* value, size_t length );
-int LTNSGetList( LTNSTerm *term, char* value, size_t *length );
-int LTNSSetListElement( LTNSTerm *term, int key, char* data, size_t length );
-int LTNSGetListElement( LTNSTerm *term, int key, char* data, size_t *length );
-
-int LTNSSetDictionary( LTNSTerm *term, char* value, size_t length );
-int LTNSGetDictionary( LTNSTerm *term, char* value, size_t*length );
-int LTNSSetDictionaryElement( LTNSTerm *term, char* key, char* data, size_t length );
-int LTNSGetDictionaryElement( LTNSTerm *term, char* key, char* data, size_t *length );
 
 #endif//__LTNSTERM_H___
