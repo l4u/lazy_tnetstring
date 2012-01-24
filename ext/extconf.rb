aa4f056c67092bef8a9d99b5effb071872de7899
Christian Lundgren <christian.lundgren@wooga.net>
2012-01-24 17:43:39 +0100

    Add ruby wrapper to autotools build chain

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -1,3 +1,11 @@
 require 'mkmf'
-$LDFLAGS  += '-lLTNS'
+$CFLAGS += ' -I../src'
 create_makefile('LazyTNetstring')
+makefile = File.read('Makefile')
+makefile.gsub!(/^SRCS.*$/) do |match|
+  "#{match} ../src/LTNSDataAccess.c ../src/LTNSTerm.c ../src/LTNSCommon.c"
+end
+makefile.gsub!(/^OBJS.*$/) do |match|
+  "#{match} ../src/LTNSDataAccess.o ../src/LTNSTerm.o ../src/LTNSCommon.o"
+end
+File.open('Makefile', 'w') {|f| f.write(makefile)}
